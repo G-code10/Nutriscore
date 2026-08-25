1e6a4a37afc484e82c1c7b8689103f8ff2ae8364
SCHROETTER Christian <schroetter.christian@wanadoo.fr>
2026-08-25 17:25:46 +0200

    Commit de fin de TP2

---
+++ src/dict_category_nutriments.py
@@ -0,0 +1,54 @@
+nutriments_categorized = dict()
+nutriments_categorized["nutriments"] = []
+nutriments_categorized["nutriments"].append({"Vitamines": [
+	"vitamin-a", 
+    "vitamin-b1", 
+    "vitamin-b2", 
+    "vitamin-b6", 
+    "vitamin-b9", 
+    "vitamin-b12", 
+    "vitamin-c", 
+    "vitamin-d", 
+    "vitamin-e", 
+    "vitamin-k", 
+    "vitamin-pp", 
+    "vitamine-h"
+	]})
+
+nutriments_categorized["nutriments"].append({"Énergie": [
+    "energy", 
+	"energy-kj", 
+	"energy-kcal"
+	]})
+
+nutriments_categorized["nutriments"].append({"Valeurs nutritionnelles": [
+    "fat", 
+    "saturated-fat", 
+    "carbohydrates", 
+    "sugars", 
+    "fiber", 
+    "proteins", 
+    "salt", 
+    "sodium", 
+    "cholesterol"
+	]})
+
+nutriments_categorized["nutriments"].append({"Minéraux": [
+    "calcium", 
+    "iron", 
+    "magnesium", 
+    "potassium", 
+    "zinc"
+	]})
+
+nutriments_categorized["nutriments"].append({"Oméga": [
+    "omega-3-fat", 
+    "omega-6-fat", 
+    "omega-9-fat"
+	]})
+
+nutriments_categorized["nutriments"].append({"Autres": [
+    "caffeine", 
+    "alcohol", 
+    "water"
+	]})
