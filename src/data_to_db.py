45329e2e799a6bdc84d10173e038090a28d0d805
Jeremy_Droulez <jeremy.droulez.pro@gmail.com>
2026-09-15 15:59:43 +0200

    BUG : 'categories_tags' ne veut pas s'étaler. Nouvelle fonction spécifique aux catégories. (+) ajout du traitement de colonnes

--- src/data_to_db.py
+++ src/data_to_db.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.append(".")
 
-from cleaning import (flatten_nutriments, flattening_column_join_and_drop)
+from cleaning import *
 from conf import sql_conf_dict as conf
 print("Import effectué")
 
@@ -20,7 +20,11 @@ conf = {
 # NSQL = nutriscore_sql.NutriscoreSQL(conf, "postgres")
 
 # TODO : 
-# Créer des fonctions, des boucles afin de pouvoir récupérer et insérer les données en BDD
+# Initialiser un fichier .parquet 
+# L'aplatir (light ou regular)
+# L'exporter
+# Le manipuler
+
 def print_df(df:pd.DataFrame, nbr_of_line:int, column:list = []) :
     if column != [] :
         if nbr_of_line != '':
@@ -34,43 +38,55 @@ def print_df(df:pd.DataFrame, nbr_of_line:int, column:list = []) :
             print(df)
 
 paquet_choice = True
-food_paquet = ''
+food_parquet = ''
 
-# Nettoyer la donnée et créer un fichier .parquet
+# Nettoyer la donnée et créer un fichier .parquet de ["code", "brands", "product_name", "nutriments"]
 while paquet_choice :
 
     choose_paquet = input("Initialiser un paquet en cleaned - light(l) ou regular(r)\nOU next(n) pour passer à la suite : ")
 
     if choose_paquet == 'light' or choose_paquet == 'l' :
-        food_paquet = "data/food_light.parquet"
+        food_parquet = "data/food_light.parquet"
     elif choose_paquet == 'regular' or choose_paquet == 'r' :
-        food_paquet = "data/food.parquet"
+        food_parquet = "data/food.parquet"
     else :
         paquet_choice = False
         break
 
     print("Récupération du parquet")
     # Récupération et nettoyage des listes : 
-    df_products = pd.read_parquet(food_paquet, columns=["code", "brands", "product_name", "nutriments"])#.head(10_000) ==> light_food
+    df_products = pd.read_parquet(food_parquet, columns=["code", "brands", "product_name", "nutriments", "categories_tags"])#.head(10_000) ==> light_food
     # df_products.to_parquet("data/food_light.parquet")
-    print(f"Parquet récupéré : {food_paquet}")
+    print(f"Parquet récupéré : {food_parquet}")
 
     print('Création de ma colonne flat_nutriments')
     df_products['flat_nutriments'] = df_products['nutriments'].apply(lambda x : flatten_nutriments(x))
     print('Colonne flat_nutriments finie')
 
-    print('Étaler flat_nutriments et suppression de celle-ci et de nutriments')
-    df_join = flattening_column_join_and_drop(df_products['flat_nutriments'], df_products, ['nutriments', 'flat_nutriments'])
+    print('Création de ma colonne flat_categories_tags')
+    df_products['flat_categories_tags'] = df_products['categories_tags'].apply(lambda x : flatten_categories(x))
+    print('Colonne flat_nutriments finie')
+
+
+    print('Étaler flat_categories_tags et suppression de celle-ci et de nutriments')
+    df_join_cat = flattening_categories_column_join_and_drop(df_products['flat_categories_tags'], df_products, ['categories_tags', 'flat_categories_tags'])
     print('Jointure et suppression finie')
 
-    print('Exportation en parquet')
-    if food_paquet == "data/food_light.parquet" :
-        df_join.to_parquet("data/cleaned_food_light.parquet")
-    elif food_paquet == "data/food.parquet" :
-        df_join.to_parquet("data/cleaned_food.parquet")
-    print('Exportation finie')
+    print('Étaler flat_nutriments et suppression de celle-ci et de nutriments')
+    df_join = flattening_nutriments_column_join_and_drop(df_products['flat_nutriments'], df_join_cat, ['nutriments', 'flat_nutriments'])
+    print('Jointure et suppression finie')
+
+    input_export = input("Voulez-vous exporter le fichier ? y / n : ")
+    if input_export.lower() == 'y' or input_export.lower() == 'yes' :
+        print('Exportation en parquet')
+        if food_parquet == "data/food_light.parquet" :
+            df_join.to_parquet("data/cleaned_food_light.parquet")
+        elif food_parquet == "data/food.parquet" :
+            df_join.to_parquet("data/cleaned_food.parquet")
+        print('Exportation finie')
 
     paquet_choice = False
+
 
 # Manipulation de la donnée
 data_choice = True
@@ -79,16 +95,16 @@ while data_choice:
     choose_paquet = input("Choisissez un parquet nettoyé : light(l) ou regular(r)\nOU quit(q) pour quitter : ")
 
     if choose_paquet == 'light' or choose_paquet == 'l' :
-        food_paquet = "data/cleaned_food_light.parquet" 
+        food_parquet = "data/cleaned_food_light.parquet" 
     elif choose_paquet == 'regular' or choose_paquet == 'r' :
-        food_paquet = "data/cleaned_food.parquet" 
+        food_parquet = "data/cleaned_food.parquet" 
     else :
         data_choice = False
         break
 
     # Charger le parquet 
-    df_products = pd.read_parquet(food_paquet)
-    print(f"Voici les colonnes du {food_paquet} :\n{df_products.columns}")
+    df = pd.read_parquet(food_parquet)
+    print(f"Voici les colonnes du {food_parquet} :\n{df.columns}")
 
     while data_choice :
 
@@ -97,7 +113,7 @@ while data_choice:
 
         while columns_continue : 
             display_df = input("Quelles colonnes voulez-vous afficher ?\nq - Quitter (dernier tour) : ")
-            if display_df.lower() in df_products.columns :
+            if display_df.lower() in df.columns :
                 columns_input.append(display_df.lower())
             elif display_df.lower() == 'q' or display_df.lower() == 'quitter':
                 columns_continue = False
@@ -109,10 +125,10 @@ while data_choice:
         nbr_of_line = -131197 
         while nbr_of_line < 0 :
             try :
-                nbr_of_line = int(input(f'Combien de lignes afficher ? (0 - {len(df_products)})\nEntrez "0" si vous ne voulez rien mettre : '))
+                nbr_of_line = int(input(f'Combien de lignes afficher ? (0 - {len(df)})\nEntrez "0" si vous ne voulez rien mettre : '))
             except :
                 continue    
             if nbr_of_line > 0 :
-                print_df(df_products,nbr_of_line, columns_input)
+                print_df(df,nbr_of_line, columns_input)
             else : 
                 data_choice = False
